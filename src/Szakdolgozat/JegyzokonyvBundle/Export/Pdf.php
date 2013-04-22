b89f8c9b1ba60f6f3039744a0fb254acf5860669
Gábor Fási <m@maerlyn.eu>
2013-04-22 19:23:17 +0200

    pdf header es footer

--- src/Szakdolgozat/JegyzokonyvBundle/Export/Pdf.php
+++ src/Szakdolgozat/JegyzokonyvBundle/Export/Pdf.php
@@ -7,9 +7,20 @@ use Szakdolgozat\JegyzokonyvBundle\Entity\Jegyzokonyv;
 
 class Pdf
 {
+    protected $templating;
+
+    public function __construct($templating)
+    {
+        $this->templating = $templating;
+    }
+
     public function jegyzokonyv(Jegyzokonyv $jegyzokonv)
     {
         $mpdf = new mPDF("utf-8");
+        $mpdf->useOddEven = false;
+
+        $mpdf->SetHTMLHeader($this->templating->render("SzakdolgozatJegyzokonyvBundle:Pdf:_header.html.twig"));
+        $mpdf->SetHTMLFooter($this->templating->render("SzakdolgozatJegyzokonyvBundle:Pdf:_footer.html.twig"));
 
         $mpdf->Output($jegyzokonv->getNev() . ".pdf", "I");
     }
